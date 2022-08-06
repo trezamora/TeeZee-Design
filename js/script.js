a15b2fbdd42f7b2402d1d5c2b3e791677f586392
trezamora <tzamora@hawaii.edu>
2022-08-05 17:25:56 -1000

    add toggle nav

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    /* TOGGLE NAV MOBILE MENU FOR SMALL SCREENS */
+    const menubuton = document.querySelector('.menu-button');
+    const menunav = document.querySelector('.toggle-nav');
+    menubuton.addEventListener('click', function () {
+        if (menunav.getAttribute('data-navstate') === 'open') {
+            /* if true do this */
+            menunav.setAttribute('data-navstate', 'closed');
+        } else {
+            /* else (if false) do this */
+            menunav.setAttribute('data-navstate', 'open');
+        };
+    });
+});
+
 var animation = bodymovin.loadAnimation({
     container: document.getElementById('anim'),
     render: 'svg',
